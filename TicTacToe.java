db65b83b3e2baddd921660757954422fc87bbc58
Bhuwanst07 <bhuwanst07@gmail.com>
2026-04-28 09:33:26 +0530

    UC7 done

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,58 +1,98 @@
+import java.util.Scanner;
 import java.util.Random;
 
 public class TicTacToe {
 
+    static char[][] board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+
+    static Random random = new Random();
+
+    // Display board
+    public static void printBoard() {
+        System.out.println("\nBoard:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(" " + board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+            if (i < 2) {
+                System.out.println("---+---+---");
+            }
+        }
+    }
+
+    // Convert slot (1–9) to row & column
+    public static int[] convertPosition(int pos) {
+        int row = (pos - 1) / 3;
+        int col = (pos - 1) % 3;
+        return new int[]{row, col};
+    }
+
+    // UC6: Place move
+    public static boolean placeMove(int row, int col, char symbol) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        if (board[row][col] != ' ') {
+            return false;
+        }
+
+        board[row][col] = symbol;
+        return true;
+    }
+
+    // UC7: Computer random move
+    public static void computerMove() {
+        int pos;
+        int row, col;
+
+        // Loop until valid move found
+        while (true) {
+            pos = random.nextInt(9) + 1; // 1–9
+            int[] rc = convertPosition(pos);
+            row = rc[0];
+            col = rc[1];
+
+            if (placeMove(row, col, 'O')) {
+                System.out.println("\nComputer chose position: " + pos);
+                break;
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
 
-        initializeBoard(board);
-        printBoard(board);
+        Scanner scanner = new Scanner(System.in);
+        int moves = 0;
 
-        tossToDecide();
-    }
+        System.out.println("=== Tic Tac Toe (You vs Computer) ===");
 
-    // UC1 methods
-    public static void initializeBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
+        while (moves < 9) {
+
+            // Human turn
+            printBoard();
+            System.out.print("\nEnter position (1-9): ");
+            int pos = scanner.nextInt();
+
+            int[] rc = convertPosition(pos);
+            if (placeMove(rc[0], rc[1], 'X')) {
+                moves++;
+            } else {
+                System.out.println("❌ Invalid move! Try again.");
+                continue;
             }
-        }
-    }
 
-    public static void printBoard(char[][] board) {
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+            if (moves >= 9) break;
 
-    // UC2: Toss logic
-    public static void tossToDecide() {
-        Random random = new Random();
-
-        int toss = random.nextInt(2); // 0 or 1
-
-        char userSymbol;
-        char computerSymbol;
-        String currentPlayer;
-
-        if (toss == 0) {
-            userSymbol = 'X';
-            computerSymbol = 'O';
-            currentPlayer = "User";
-        } else {
-            userSymbol = 'O';
-            computerSymbol = 'X';
-            currentPlayer = "Computer";
+            // Computer turn
+            computerMove();
+            moves++;
         }
 
-        System.out.println("\nToss Result:");
-        System.out.println("User Symbol: " + userSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
-        System.out.println("First Turn: " + currentPlayer);
+        printBoard();
+        System.out.println("\nGame Over!");
+        scanner.close();
     }
 }
