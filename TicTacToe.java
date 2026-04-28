304f9cfcc64fe5fbb61692ebce7bd2a58cb11a40
Bhuwanst07 <bhuwanst07@gmail.com>
2026-04-28 09:32:06 +0530

    UC6 done

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,18 +1,15 @@
-import java.util.Random;
-
 public class TicTacToe {
 
+    static char[][] board = new char[3][3];
+
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        initializeBoard();
 
-        initializeBoard(board);
-        printBoard(board);
-
-        tossToDecide();
+        placeMove(0, 0, 'X');
+        System.out.println(board[0][0]);
     }
 
-    // UC1 methods
-    public static void initializeBoard(char[][] board) {
+    static void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = '-';
@@ -20,39 +17,7 @@ public class TicTacToe {
         }
     }
 
-    public static void printBoard(char[][] board) {
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    // UC2: Toss logic
-    public static void tossToDecide() {
-        Random random = new Random();
-
-        int toss = random.nextInt(2); // 0 or 1
-
-        char userSymbol;
-        char computerSymbol;
-        String currentPlayer;
-
-        if (toss == 0) {
-            userSymbol = 'X';
-            computerSymbol = 'O';
-            currentPlayer = "User";
-        } else {
-            userSymbol = 'O';
-            computerSymbol = 'X';
-            currentPlayer = "Computer";
-        }
-
-        System.out.println("\nToss Result:");
-        System.out.println("User Symbol: " + userSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
-        System.out.println("First Turn: " + currentPlayer);
+    static void placeMove(int row, int col, char symbol) {
+        board[row][col] = symbol;
     }
 }
