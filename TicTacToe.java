29161afb66cd92d532ee783b051a5cee3cfaae5d
Bhuwanst07 <bhuwanst07@gmail.com>
2026-04-28 09:29:34 +0530

    UC4 done

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,58 +1,20 @@
-import java.util.Random;
-
 public class TicTacToe {
 
     public static void main(String[] args) {
-        char[][] board = new char[3][3];
+        int slot = 7; // Example input
 
-        initializeBoard(board);
-        printBoard(board);
+        int[] position = convertSlotToPosition(slot);
 
-        tossToDecide();
+        System.out.println("Slot: " + slot);
+        System.out.println("Row: " + position[0]);
+        System.out.println("Column: " + position[1]);
     }
 
-    // UC1 methods
-    public static void initializeBoard(char[][] board) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                board[i][j] = '-';
-            }
-        }
-    }
+    // UC4: Convert slot (1–9) to row & column (0–2)
+    public static int[] convertSlotToPosition(int slot) {
+        int row = (slot - 1) / 3;
+        int col = (slot - 1) % 3;
 
-    public static void printBoard(char[][] board) {
-        System.out.println("Tic-Tac-Toe Board:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    // UC2: Toss logic
-    public static void tossToDecide() {
-        Random random = new Random();
-
-        int toss = random.nextInt(2); // 0 or 1
-
-        char userSymbol;
-        char computerSymbol;
-        String currentPlayer;
-
-        if (toss == 0) {
-            userSymbol = 'X';
-            computerSymbol = 'O';
-            currentPlayer = "User";
-        } else {
-            userSymbol = 'O';
-            computerSymbol = 'X';
-            currentPlayer = "Computer";
-        }
-
-        System.out.println("\nToss Result:");
-        System.out.println("User Symbol: " + userSymbol);
-        System.out.println("Computer Symbol: " + computerSymbol);
-        System.out.println("First Turn: " + currentPlayer);
+        return new int[]{row, col};
     }
 }
